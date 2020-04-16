2db2b6702a3d3f5b32618e9ac81dcf58e690fb9c
Keinier Caboverde <keinier@gmail.com>
2020-04-16 17:11:19 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,57 @@
+// Recita un poema 
+// param: @nombre: Nombre de la persona
+// 
+const KeinierRecita = (nombre) => {
+
+	const poema = `
+
+	Es su voz un torrente de armonía,
+	y fulgura en su espléndida mirada,
+	de sus propios encantos encantada,
+	la cara de luz con que despunta el día.
+
+	Al leve junco vence en gentileza,
+	Y a Venus en beldad. Y se llamó ${nombre}.
+
+	¡Verla sentada! ¡Mírala! ¡Es ella!
+	¡Ve cuán gentil! ¡Entre cojines rojos!
+	¡Con qué graciosa languidez, tan bella!
+	¡Con el alma jovial, fuente de amores,
+	en las negras pupilas de sus ojos!...
+
+	Cual un rayo de luz... Entre dos flores 
+
+          love love l                ove love lo
+      ve love love love           love love love lov
+   e love love love love l      ove love love love love
+   love love love love love     love love love love love
+  love love love love love l  ove love love love love lov
+ e love love love love love love love love love love love
+  love love love love love love love love love love love
+  love love love love love love love love love love love
+   love love love love love love love love love love lov
+    e love love love love love love love love love lov
+      e love love love love love love love love love
+         love love love love love love love love l
+          ove love love love love love love love
+             love love love love love love lov
+              e love love love love love lov
+                e love love love love love
+                   love love love love lo
+                    ve love love love lo
+                      ve love love love
+                       love love love
+                         love love
+                          love lo
+                            ve
+	`;
+	console.clear();
+	console.log(poema);
+	return poema;
+}
+
+// Te amo Nilza Rodríguez...
+KeinierRecita("Nilza");
+
+
+
